166987b75649a9dff7c32400d5c3c2d6a5df82c1
Henrik Mårtenzon <henrikmartenzon@gmail.com>
2018-12-30 13:57:23 +0100

    Cleanup

--- appz/martenzi-defaults-write.sh
+++ appz/martenzi-defaults-write.sh
@@ -184,6 +184,6 @@ killall cfprefsd
 ###############################################################################
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
   "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
-  "iCal" "Terminal"; do
-  killall "${app}" > /dev/null 2>&1
+  "iCal" "Terminal"; \
+  do killall "${app}" > /dev/null 2>&1
 done;
